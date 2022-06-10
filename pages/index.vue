6d70d3f8a33d7eefbcb80576c9e4b6d090122122
ivan mufid <ivanmohammad2355@gmail.com>
2022-06-10 13:49:22 +0700

    login form

---
+++ pages/index.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="page">
+    <div class="container text-center">
+      <br><br><br>
+      <h1 class="title">
+        <strong>S I M A R S</strong>
+      </h1>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage',
+
+}
+</script>
+
+<style>
+.page{
+    align-items: center;
+    justify-content: center;
+    height: 100vh;
+    background-image: url("~/assets/img/bg.png");
+    background-repeat: no-repeat;
+    background-size:contain;
+    background-position:center;
+    background-color: #0957DE;
+}
+.title{
+  color: white;
+  font-weight: bold;
+  font-size: 5vw;
+}
+</style>
