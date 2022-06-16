a6bd100f62f8c3c9f5fa0d21162cf029f9c4ef3f
ivan mufid <ivanmohammad2355@gmail.com>
2022-06-16 22:21:01 +0700

    -gap

---
+++ pages/nomorAntrian.vue
@@ -0,0 +1,88 @@
+<template>
+  <div class="page">
+    <div class="container-fluid text-center">
+      <h1>PENDAFTARAN BERHASIL</h1>
+        <div class="card mt-5">
+          <div class="card-title">
+            No Antrian
+          </div>
+          <div class="card-body">
+            <div class="content">
+              <h1 class="mt-5 mb-5">
+                <strong>UM01</strong>
+              </h1>
+            </div>
+            <div class="content mt-3">
+              <h4>Dokter</h4>
+              <h1>
+                <strong>dr.Liza Amalia Putri</strong>
+              </h1>
+            </div>
+            <div class="mt-3">
+              <div class="container">
+                <div class="row gap">
+                  <div class="content col-6">
+                    <h4>Dokter</h4>
+                    <h3>
+                      <strong>dr.Liza Amalia Putri</strong>
+                    </h3>
+                  </div>
+                  <div class="content col-6">
+                    <h4>Dokter</h4>
+                    <h3>
+                      <strong>dr.Liza Amalia Putri</strong>
+                    </h3>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+    </div>
+  </div>
+</template>
+
+<style>
+.page{
+  background-image: url("~/assets/img/bgantrian.png");
+  height: 100vh;
+  background-size: cover;
+  display: flex;
+  margin: 0px;
+}
+.container-fluid{
+  margin: auto;
+  margin-top: 150px;
+  width: 800px;
+}
+.card{
+  border: none;
+  border-radius: 10px;
+}
+.card-title{
+  background-color: #70a4fd;
+  color: white;
+  font-size: 2em;
+  font-weight: bold;
+  border-radius: 10px 10px 0 0;
+}
+.content{
+  padding: 5px;
+  border-style: groove;
+  border-color: black;
+  border-width: thin;
+  border-radius: 5px;
+}
+.content.col-6{
+  padding: 5px;
+  border-style: groove;
+  border-color: black;
+  border-width: thin;
+  border-radius: 5px;
+  width: 100px;
+}
+.gap{
+  column-count: 2;
+  column-gap: 40px;
+}
+</style>
